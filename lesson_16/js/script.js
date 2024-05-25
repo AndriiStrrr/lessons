cf2af76e29e9755da55708df5917b7bb887b561c
AndriiStrrr <143613997+AndriiStrrr@users.noreply.github.com>
2024-05-25 19:30:14 +0300

    Add files via upload

--- lesson_16/js/script.js
+++ lesson_16/js/script.js
@@ -1,8 +1,13 @@
+document.addEventListener("click", function (e) {
+  const targetElement = e.target;
+  if (targetElement.closest(".menu-icon")) {
+    document.documentElement.classList.toggle("menu-open");
+  }
+});
 
-
-document.addEventListener('click', function (e) {
-	const targetElement = e.target;
-	if (targetElement.closest('.menu-icon')) {
-		document.documentElement.classList.toggle('menu-open');
-	}
-})
+document.addEventListener("click", function (e) {
+  const targetElement = e.target;
+  if (targetElement.closest(".contacts-mid-header__icon")) {
+    document.documentElement.classList.toggle("popup-phones");
+  }
+});
